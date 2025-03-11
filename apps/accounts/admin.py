74a3a18e51067b0e63eea999d1dd96ca9ee38899
komronium <komronium@gmail.com>
2025-03-11 18:24:59 +0500

    add role to user admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.safestring import mark_safe
+from django.contrib.admin import SimpleListFilter
 
 from apps.accounts.models import User
 
+
+class RoleFilter(SimpleListFilter):
+    title = 'Роль'
+    parameter_name = 'role'
+
+    def lookups(self, request, model_admin):
+        return [
+            ['partner', 'партнер'],
+            ['client', 'клиент']
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'partner':
+            return queryset.filter(is_partner=True)
+        if self.value() == 'client':
+            return queryset.filter(is_partner=False)
+        return queryset
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'full_name', 'is_partner', 'phone_number')
     readonly_fields = ('date_joined',)
     ordering = ('-date_joined',)
+    list_filter = [RoleFilter]
     fieldsets = (
         (None, {'fields': (
             'email', 'full_name', 'phone_number', 'is_partner', 'inn'
